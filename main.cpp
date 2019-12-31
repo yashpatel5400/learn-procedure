8f6c8e92300912f2dabea9821af3734a654dafcc
Yash Patel <ypatel@fb.com>
2019-12-31 14:53:11 -0500

    Several bugfixes -- running now. Need to add drawing output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <iostream
+#include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <math.h>
@@ -20,18 +21,24 @@ const float density = 1.0;
 std::vector<float> bilinear(
         const VectorField& grid,
         const std::pair<float, float>& pos) {
-    int x0 = int(pos[0]);
-    int y0 = int(pos[1]);
-    int x1 = x0 + 1;
-    int y1 = y0 + 1;
+    int height = grid.size();
+    int width = grid[0].size();
 
-    float xPercentile = (pos[0] - x0) / float(x1 - x0);
-    float yPercentile = (pos[1] - y0) / float(y1 - y0);
+    float x = std::clamp(pos.first, float(0.), float(width) - 1);
+    float y = std::clamp(pos.second, float(0.), float(height) - 1);
 
-    float v00 = grid[y0][x0];
-    float v01 = grid[y0][x1];
-    float v10 = grid[y1][x0];
-    float v11 = grid[y1][x1];
+    int x0 = std::clamp(int(x), 0, width - 1);
+    int y0 = std::clamp(int(y), 0, height - 1);
+    int x1 = std::clamp(x0 + 1, 0, width - 1);
+    int y1 = std::clamp(y0 + 1, 0, height - 1);
+
+    float xPercentile = (x - x0) / float(x1 - x0);
+    float yPercentile = (y - y0) / float(y1 - y0);
+
+    auto v00 = grid[y0][x0];
+    auto v01 = grid[y0][x1];
+    auto v10 = grid[y1][x0];
+    auto v11 = grid[y1][x1];
 
     std::vector<float> result;
     for (int i = 0; i < v00.size(); i++) {
@@ -45,43 +52,51 @@ std::vector<float> bilinear(
 
 ScalarField calcDiv(const VectorField&  advectVelocity) {
     ScalarField divergence;
+    int height = advectVelocity.size();
+    int width = advectVelocity[0].size();
+
     for (int row = 0; row < height; row++) {
-        std::vector<float> row;
+        std::vector<float> divRow;
         for (int col = 0; col < width; col++) {
             float div = -2.0 * epsilon * density / timeStep * (
-                advectVelocity[col + 1][row] -
-                advectVelocity[col - 1][row] +
-                advectVelocity[col][row + 1] -
-                advectVelocity[col][row - 1]
+                advectVelocity[row][(col + 1) % width][0] -
+                advectVelocity[row][((col - 1) + width) % width][0] +
+                advectVelocity[(row + 1) % height][col][1] -
+                advectVelocity[((row - 1) + height) % height][col][1]
             );
-            row.push_back(div);
+            divRow.push_back(div);
         }
-        divergence.push_back(row);
+        divergence.push_back(divRow);
     }
     return divergence;
 }
 
 ScalarField calcPressure(const ScalarField& divergence) {
     ScalarField pressure;
+    int height = divergence.size();
+    int width = divergence[0].size();
+
+    std::cout << height << " " << width << std::endl;
+
     for (int row = 0; row < height; row++) {
-        std::vector<float> row(width, 0.0);
-        pressure.push_back(row);
+        std::vector<float> pressureRow(width, 0.0);
+        pressure.push_back(pressureRow);
     }
 
     for (int jacobiIter = 0; jacobiIter < jacobiIters; jacobiIter++) {
-        std::vector<float> updatedPressure;
+        ScalarField updatedPressure;
         for (int row = 0; row < height; row++) {
-            std::vector<float> row;
+            std::vector<float> pressureRow;
             for (int col = 0; col < width; col++) {
                 float p = divergence[row][col] + 1.0 / 4.0 * (
-                    pressure[row + 2][col] +
-                    pressure[row - 2][col] +
-                    pressure[row][col + 2] +
-                    pressure[row][col - 2]
+                    pressure[(row + 2) % height][col] +
+                    pressure[((row - 2) + height) % height][col] +
+                    pressure[row][(col + 2) % width] +
+                    pressure[row][((col - 2) + width) % width]
                 );
-                row.push_back(p);
+                pressureRow.push_back(p);
             }
-            updatedPressure.push_back(row);
+            updatedPressure.push_back(pressureRow);
         }
         pressure = updatedPressure;
     }
@@ -95,12 +110,19 @@ int main() {
     VectorField color;
     VectorField velocity;
     for (int row = 0; row < height; row++) {
-        std::vector<float> velRow;
-        std::vector<float> colRow;
+        std::vector<std::vector<float> > velRow;
+        std::vector<std::vector<float> > colRow;
 
         for (int col = 0; col < width; col++) {
-            velRow.emplace_back({sin(2 * PI * row), sin(2 * PI * col)});
-            colRow.emplace_back({abs(sin(2 * PI * row)), abs(cos(2 * PI * col)), abs(cos(2 * PI * col))});
+            velRow.push_back({
+                static_cast<float>(sin(2 * PI * row)),
+                static_cast<float>(sin(2 * PI * col))
+            });
+            colRow.push_back({
+                static_cast<float>(abs(sin(2 * PI * row))),
+                static_cast<float>(abs(cos(2 * PI * col))),
+                static_cast<float>(abs(cos(2 * PI * col)))
+            });
         }
 
         velocity.push_back(velRow);
@@ -110,12 +132,12 @@ int main() {
     for (int iter = 0; iter < iters; iter++) {
         VectorField advectVelocity;
         for (int row = 0; row < height; row++) {
-            std::vector<float> row;
+            std::vector<std::vector<float> > advRow;
             for (int col = 0; col < width; col++) {
                 std::pair<float, float> pos(col - velocity[row][col][0] * timeStep, row - velocity[row][col][1] * timeStep);
-                row.push_back(bilinear(velocity, pos));
+                advRow.push_back(bilinear(velocity, pos));
             }
-            advectVelocity.push_back(row);
+            advectVelocity.push_back(advRow);
         }
 
         auto divergence = calcDiv(advectVelocity);
@@ -123,13 +145,13 @@ int main() {
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                float update = timeStep / (2.0 * density * epsilon) * (pressure[row][col + 1] - pressure[row][col - 1]);
+                float coeff = timeStep / (2.0 * density * epsilon);
                 velocity[row][col] = {
-                    advectVelocity[row][col][0] - update,
-                    advectVelocity[row][col][1] - update,
+                    advectVelocity[row][col][0] - coeff * (pressure[row][(col + 1) % width] - pressure[row][((col - 1) + width) % width]),
+                    advectVelocity[row][col][1] - coeff * (pressure[(row + 1) % height][col] - pressure[((row - 1) + height) % height][col]),
                 };
 
-                std::pair<float, float> pos = (col - velocity[row][col][0] * timeStep, row - velocity[row][col][1] * timeStep);
+                std::pair<float, float> pos = {col - velocity[row][col][0] * timeStep, row - velocity[row][col][1] * timeStep};
                 color[row][col] = bilinear(color, pos);
             }
         }
